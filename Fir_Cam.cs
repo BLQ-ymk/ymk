67aff2bd97cf57feb3b6424800845d16b0e2395e
BLQ-ymk <48340485+BLQ-ymk@users.noreply.github.com>
2019-03-10 19:13:35 +0800

    Create Fir_Cam.cs
    yyyy

---
+++ Fir_Cam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fir_Cam : MonoBehaviour {
+
+    public GameObject targetobj;
+    public float mousesensity;
+    public float MinY=-40f;
+    public float MaxY=40f;
+    float Camera_X;
+    float Camera_Y;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Camera_X = transform.localEulerAngles.y+Input.GetAxis("Mouse X") * mousesensity;
+        Camera_Y+= Input.GetAxis("Mouse Y") * mousesensity;
+        Camera_Y = Mathf.Clamp(Camera_Y,MinY,MaxY);
+        transform.localEulerAngles = new Vector3(-Camera_Y,Camera_X,0f);
+        transform.position = targetobj.transform.position;
+	}
+}
